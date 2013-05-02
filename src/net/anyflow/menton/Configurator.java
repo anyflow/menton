cffe25d6bd9013e7f1df83619b4a6fa1b5ec63a2
Park Hyunjeong <anyflow@gmail.com>
2013-05-02 23:03:31 +0900

    RESOLVED - issue NE-20: Changing http request handler root location(from config file to java)  https://anyflow.atlassian.net/browse/NE-20

--- src/net/anyflow/menton/Configurator.java
+++ src/net/anyflow/menton/Configurator.java
@@ -3,98 +3,93 @@
  */
 package net.anyflow.menton;
 
-import java.net.URISyntaxException;
-
 import net.anyflow.menton.exception.DefaultException;
-
 
 /**
  * @author anyflow
- *
  */
 public class Configurator {
-	
+
 	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(Configurator.class);
 	private static java.util.Properties configuration;
-	
-	/**
-	 * initialize the configurator with the default properties file which should be located in the same path with the jar file.
-	 */
-	public static void initialize() {
-		try {
-			String configPath = (new java.io.File(Configurator.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath())).getParentFile().getPath();
-			initialize(configPath + "/facade.properties");
-		}
-		catch (URISyntaxException e) {
-			logger.error("Getting the configuration file path has failed.", e);
-		}	
-	}
-	
+
 	/**
 	 * initialize the configurator
-	 * @param propertyFilePath full path and file name of the properties file.
+	 * 
+	 * @param propertyFilePath
+	 *            full path and file name of the properties file.
+	 * @param httpRequestHandlerPackageRoot
+	 *            null in case of no using http service.
 	 */
 	public static void initialize(String propertyFilePath) {
-		
+
 		if(configuration == null) {
 			configuration = new java.util.Properties();
 		}
-		
+
 		try {
 			java.io.InputStream is = new java.io.FileInputStream(propertyFilePath);
-			
+
 			configuration.load(is);
 		}
-		catch (java.io.FileNotFoundException e) {
+		catch(java.io.FileNotFoundException e) {
 			logger.error("Error occured on finding configuration file path.", e);
 		}
-		catch (java.io.IOException e) {
+		catch(java.io.IOException e) {
 			logger.error("Loading network properties failed.", e);
 		}
 	}
-	
+
+	public static void setRequestHandlerPackageRoot(String requestHandlerPackageRoot) throws DefaultException {
+		if(configuration == null) { throw new DefaultException("Configurator.configure should be called before this."); }
+
+		configuration.setProperty("httpServer.requestHandlerPackageRoot", requestHandlerPackageRoot);
+	}
+
 	/**
 	 * @return Request Handler Package root name
 	 * @throws DefaultException
 	 */
 	public static String getRequestHandlerPackageRoot() throws DefaultException {
-		if(configuration == null) { throw new DefaultException("Configurator.configure should be called before this."); } 
-		
-		return configuration.getProperty("httpServer.requestHandlerPackageRoot", "requestHandler Package key was not found");
+		if(configuration == null) { throw new DefaultException("Configurator.configure should be called before this."); }
+
+		return configuration.getProperty("httpServer.requestHandlerPackageRoot",
+				"requestHandler Package key was not found");
 	}
+
 	/**
 	 * @return http port
 	 * @throws DefaultException
 	 */
 	public static int getHttpPort() throws DefaultException {
-		if(configuration == null) { throw new DefaultException("Configurator.configure should be called before this."); } 
-		
+		if(configuration == null) { throw new DefaultException("Configurator.configure should be called before this."); }
+
 		return Integer.parseInt(configuration.getProperty("httpServer.port", "8090"));
 	}
-	
+
 	/**
 	 * @return avro port
 	 * @throws DefaultException
 	 */
 	public static int getAvroPort() throws DefaultException {
-		if(configuration == null) { throw new DefaultException("Configurator.configure should be called before this."); } 
-		
+		if(configuration == null) { throw new DefaultException("Configurator.configure should be called before this."); }
+
 		return Integer.parseInt(configuration.getProperty("avroServer.port", "9090"));
 	}
-	
+
 	/**
 	 * @return context root path
 	 * @throws DefaultException
 	 */
 	public static String getHttpContextRoot() throws DefaultException {
-		if(configuration == null) { throw new DefaultException("Configurator.configure should be called before this."); } 
-		
+		if(configuration == null) { throw new DefaultException("Configurator.configure should be called before this."); }
+
 		String ret = configuration.getProperty("httpServer.contextRoot", "/");
-		
+
 		if(ret.equalsIgnoreCase("") || ret.charAt(ret.length() - 1) != '/') {
 			ret += "/";
 		}
-		
+
 		return ret;
 	}
 }
--- src/net/anyflow/menton/http/DefaultHttpServerHandler.java
+++ src/net/anyflow/menton/http/DefaultHttpServerHandler.java
@@ -1,6 +1,5 @@
 package net.anyflow.menton.http;
 
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URI;
@@ -11,7 +10,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-
+import net.anyflow.menton.Configurator;
 import net.anyflow.menton.exception.DefaultException;
 
 import org.jboss.netty.buffer.ChannelBuffers;
@@ -31,60 +30,59 @@ import org.jboss.netty.handler.codec.http.HttpResponseStatus;
 import org.jboss.netty.handler.codec.http.HttpVersion;
 import org.jboss.netty.util.CharsetUtil;
 
-
 public class DefaultHttpServerHandler extends SimpleChannelUpstreamHandler {
-	
+
 	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(DefaultHttpServerHandler.class);
 
 	private RequestHandler requestHandler;
-	
-	public DefaultHttpServerHandler() {
+
+	public DefaultHttpServerHandler(String requestHandlerPackageRoot) throws DefaultException {
+		Configurator.setRequestHandlerPackageRoot(requestHandlerPackageRoot);
 	}
 
 	public DefaultHttpServerHandler(RequestHandler requestHandler) {
 		this.requestHandler = requestHandler;
 	}
 
-    public void messageReceived(ChannelHandlerContext ctx, final MessageEvent e) throws Exception {
-    	
-    	final HttpRequest request = (HttpRequest)e.getMessage();
-        final HttpResponse response = createDefaultResponse(request);
+	@Override
+	public void messageReceived(ChannelHandlerContext ctx, final MessageEvent e) throws Exception {
 
-        //TODO Replace multithreadings with typical proxy code.
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        Future<String> future = executorService.submit(new Callable<String>() {
+		final HttpRequest request = (HttpRequest)e.getMessage();
+		final HttpResponse response = createDefaultResponse(request);
+
+		// TODO Replace multithreadings with typical proxy code.
+		ExecutorService executorService = Executors.newSingleThreadExecutor();
+		Future<String> future = executorService.submit(new Callable<String>() {
 
 			@Override
-			public String call() 
-			{
+			public String call() {
 				logger.info(request.getUri().toString() + " requested.");
-				
+
 				try {
 					String path = (new URI(request.getUri())).getPath();
 
-					return requestHandler != null
-						   ? handleRequestByMethodTypeHandler(e, request, response, path)
-						   : handleRequestByClassTypeHandler(e, request, response, path);
+					return requestHandler != null ? handleRequestByMethodTypeHandler(e, request, response, path)
+							: handleRequestByClassTypeHandler(e, request, response, path);
 				}
-				catch (URISyntaxException e1) {
-		    		response.setStatus(HttpResponseStatus.NOT_FOUND);
-		    		logger.info("unexcepted URI : {}", request.getUri().toString(), e);
-		    		return null;
+				catch(URISyntaxException e1) {
+					response.setStatus(HttpResponseStatus.NOT_FOUND);
+					logger.info("unexcepted URI : {}", request.getUri().toString(), e);
+					return null;
 				}
-				catch (InstantiationException e) {
+				catch(InstantiationException e) {
 					response.setStatus(HttpResponseStatus.INTERNAL_SERVER_ERROR);
-		    		logger.error("Generating new request handler instance failed.", e);
-		    		return null;
+					logger.error("Generating new request handler instance failed.", e);
+					return null;
 				}
-				catch (IllegalAccessException e) {
+				catch(IllegalAccessException e) {
 					response.setStatus(HttpResponseStatus.INTERNAL_SERVER_ERROR);
-		    		logger.error("Generating new request handler instance failed.", e);
-		    		return null;
+					logger.error("Generating new request handler instance failed.", e);
+					return null;
 				}
 				catch(Exception e) {
 					response.setStatus(HttpResponseStatus.INTERNAL_SERVER_ERROR);
-		    		logger.error("Unknown exception was thrown", e);
-		    		return null;
+					logger.error("Unknown exception was thrown", e);
+					return null;
 				}
 			}
 
@@ -98,19 +96,17 @@ public class DefaultHttpServerHandler extends SimpleChannelUpstreamHandler {
 			 * @throws IllegalAccessException
 			 * @throws InvocationTargetException
 			 */
-			private String handleRequestByMethodTypeHandler(
-					final MessageEvent e, final HttpRequest request,
-					final HttpResponse response, String path)
-					throws DefaultException, IllegalAccessException,
+			private String handleRequestByMethodTypeHandler(final MessageEvent e, final HttpRequest request,
+					final HttpResponse response, String path) throws DefaultException, IllegalAccessException,
 					InvocationTargetException {
-				
+
 				requestHandler.initialize(request, response);
 				Method handler = requestHandler.findHandler(path, request.getMethod().toString());
-				
+
 				if(handler == null) {
 					response.setStatus(HttpResponseStatus.NOT_FOUND);
 					logger.info("unexcepted URI : {}", request.getUri().toString(), e);
-					
+
 					return "Failed to find the request handler.";
 				}
 				else {
@@ -128,92 +124,89 @@ public class DefaultHttpServerHandler extends SimpleChannelUpstreamHandler {
 			 * @throws InstantiationException
 			 * @throws IllegalAccessException
 			 */
-			private String handleRequestByClassTypeHandler(
-					final MessageEvent e, final HttpRequest request,
-					final HttpResponse response, String path)
-					throws DefaultException, InstantiationException,
+			private String handleRequestByClassTypeHandler(final MessageEvent e, final HttpRequest request,
+					final HttpResponse response, String path) throws DefaultException, InstantiationException,
 					IllegalAccessException {
-				Class<? extends RequestHandler> handlerClass = RequestHandler.find(path, request.getMethod().toString());
-				
+				Class<? extends RequestHandler> handlerClass = RequestHandler
+						.find(path, request.getMethod().toString());
+
 				if(handlerClass == null) {
 					response.setStatus(HttpResponseStatus.NOT_FOUND);
 					logger.info("unexcepted URI : {}", request.getUri().toString(), e);
-					
+
 					return "Failed to find the request handler.";
 				}
 				else {
 					RequestHandler handler = handlerClass.newInstance();
-					
+
 					handler.initialize(request, response);
-					
+
 					return handler.call();
 				}
 			}
-        });
-        
-        String content = future.get();
-        executorService.shutdownNow();
-        
-        writeResponse(response, normalize(content), HttpHeaders.isKeepAlive(request), e);
-    }
-    
-    private String normalize(String content) {
-    	//TODO normalize content.. but for what?
-    	
-    	return content;
-    }
-    
-    private HttpResponse createDefaultResponse(HttpRequest request) {
-        // Build the response object.
-        HttpResponse ret = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
-        
-        ret.setHeader(HttpHeaders.Names.CONTENT_TYPE, "application/json; charset=UTF-8");
+		});
 
-        //resolve cross domain issue
-        ret.setHeader("Access-Control-Allow-Origin", "*");
-        ret.setHeader("Access-Control-Allow-Methods", "POST, GET");
-        ret.setHeader("Access-Control-Allow-Headers", "X-PINGARUNER");
-        ret.setHeader("Access-Control-Max-Age", "1728000");
-        
-        String cookieString = request.getHeader(HttpHeaders.Names.COOKIE);
-        if(cookieString == null || cookieString.isEmpty()) { 
-        	return ret;
-        }
-        
-        CookieDecoder cookieDecoder = new CookieDecoder();
-        Set<Cookie> cookies = cookieDecoder.decode(cookieString);
-        if (cookies.isEmpty()) {
-        	return ret;
-        }
+		String content = future.get();
+		executorService.shutdownNow();
 
-        // Reset the cookies if necessary.
-        CookieEncoder cookieEncoder = new CookieEncoder(true);
-        for (Cookie cookie : cookies) {
-            cookieEncoder.addCookie(cookie);
-            ret.addHeader(HttpHeaders.Names.SET_COOKIE, cookieEncoder.encode());
-        }
-        
-        return ret;
-    }
-    
-    private void writeResponse(HttpResponse response, String content, boolean isKeepAlive, MessageEvent e) {
-    	
-        response.setContent(ChannelBuffers.copiedBuffer(content == null ? "" : content, CharsetUtil.UTF_8));
+		writeResponse(response, normalize(content), HttpHeaders.isKeepAlive(request), e);
+	}
 
-        if (isKeepAlive) {
-            // Add 'Content-Length' header only for a keep-alive connection.
-            response.setHeader(HttpHeaders.Names.CONTENT_LENGTH, response.getContent().readableBytes());
-            // Add keep alive header as per:
-            // - http://www.w3.org/Protocols/HTTP/1.1/draft-ietf-http-v11-spec-01.html#Connection
-            response.setHeader(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
-        }
+	private String normalize(String content) {
+		// TODO normalize content.. but for what?
 
-        // Write the response.
-        ChannelFuture future = e.getChannel().write(response);
+		return content;
+	}
 
-        // Close the non-keep-alive connection after the write operation is done.
-        if (!isKeepAlive) {
-            future.addListener(ChannelFutureListener.CLOSE);
-        }
-    }
+	private HttpResponse createDefaultResponse(HttpRequest request) {
+		// Build the response object.
+		HttpResponse ret = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+
+		ret.setHeader(HttpHeaders.Names.CONTENT_TYPE, "application/json; charset=UTF-8");
+
+		// resolve cross domain issue
+		ret.setHeader("Access-Control-Allow-Origin", "*");
+		ret.setHeader("Access-Control-Allow-Methods", "POST, GET");
+		ret.setHeader("Access-Control-Allow-Headers", "X-PINGARUNER");
+		ret.setHeader("Access-Control-Max-Age", "1728000");
+
+		String cookieString = request.getHeader(HttpHeaders.Names.COOKIE);
+		if(cookieString == null || cookieString.isEmpty()) { return ret; }
+
+		CookieDecoder cookieDecoder = new CookieDecoder();
+		Set<Cookie> cookies = cookieDecoder.decode(cookieString);
+		if(cookies.isEmpty()) { return ret; }
+
+		// Reset the cookies if necessary.
+		CookieEncoder cookieEncoder = new CookieEncoder(true);
+		for(Cookie cookie : cookies) {
+			cookieEncoder.addCookie(cookie);
+			ret.addHeader(HttpHeaders.Names.SET_COOKIE, cookieEncoder.encode());
+		}
+
+		return ret;
+	}
+
+	private void writeResponse(HttpResponse response, String content, boolean isKeepAlive, MessageEvent e) {
+
+		response.setContent(ChannelBuffers.copiedBuffer(content == null ? "" : content, CharsetUtil.UTF_8));
+
+		if(isKeepAlive) {
+			// Add 'Content-Length' header only for a keep-alive connection.
+			response.setHeader(HttpHeaders.Names.CONTENT_LENGTH, response.getContent().readableBytes());
+			// Add keep alive header as per:
+			// -
+			// http://www.w3.org/Protocols/HTTP/1.1/draft-ietf-http-v11-spec-01.html#Connection
+			response.setHeader(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+		}
+
+		// Write the response.
+		ChannelFuture future = e.getChannel().write(response);
+
+		// Close the non-keep-alive connection after the write operation is
+		// done.
+		if(!isKeepAlive) {
+			future.addListener(ChannelFutureListener.CLOSE);
+		}
+	}
 }
