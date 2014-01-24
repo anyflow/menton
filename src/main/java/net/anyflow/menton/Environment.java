276a7159e569f098449957d6e1786b3164146516
Park Hyunjeong <anyflow@gmail.com>
2014-01-25 01:50:06 +0900

    versioning completed.

--- src/main/java/net/anyflow/menton/Environment.java
+++ src/main/java/net/anyflow/menton/Environment.java
@@ -3,8 +3,9 @@
  */
 package net.anyflow.menton;
 
+
 public class Environment {
 	
-	public final static String PROJECT_ARTIFACT_ID = "@project.artifactId@";
-	public final static String PROJECT_VERSION = "@project.version@";
+	public final static String PROJECT_ARTIFACT_ID = "menton";
+	public final static String PROJECT_VERSION = "1.4.5";
 }
