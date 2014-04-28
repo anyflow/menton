3956a4e68340fcc74a49ae08a1d970dc89c627be
Park Hyunjeong <anyflow@gmail.com>
2014-04-28 14:12:16 +0900

    example.twitter list api unit testcase updated.

---
+++ src/test/java/net/anyflow/menton/example/twitter/ListTest.java
@@ -0,0 +1,57 @@
+package net.anyflow.menton.example.twitter;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.util.CharsetUtil;
+
+import java.net.URISyntaxException;
+import java.util.List;
+
+import net.anyflow.menton.http.HttpClient;
+import net.anyflow.menton.http.HttpResponse;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import com.jayway.jsonpath.JsonPath;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ListTest extends ApiTestCase {
+
+	@BeforeClass
+	public static void setup() throws UnsupportedOperationException, URISyntaxException {
+		TweetTest tt = new TweetTest();
+		
+		for(int i = 0; i < count; ++i) {
+			tt.test1_PUT();
+		}
+	}
+
+	@AfterClass
+	public static void teardown() {
+
+	}
+
+	final String address = "http://localhost:" + ApiTestSuite.server().port() + "/twitter/list";
+	final static int count = 100;
+	
+	@Test
+	public void test1_PUT() throws UnsupportedOperationException, URISyntaxException {
+
+		HttpClient client = new HttpClient(address);
+
+		HttpResponse response = client.get();
+
+		assertThat(response.getStatus(), is(HttpResponseStatus.OK));
+
+		String content = response.content().toString(CharsetUtil.UTF_8);
+
+		List<Object> list = JsonPath.read(content,  "$.");
+		
+		assertThat(list.size(), is(count));
+	}
+}
